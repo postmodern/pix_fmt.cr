c23a3919eb9f7fbdf3778b40f11a338b852f1217
Postmodern <postmodern.mod3@gmail.com>
2021-03-27 22:56:26 -0700

    Finally add HSVA (aka HSV32).

---
+++ src/pix_fmt/hsva.cr
@@ -0,0 +1,29 @@
+require "./fields/h"
+require "./fields/s"
+require "./fields/a"
+
+module PixFmt
+  struct HSVA
+
+    include Fields::H(UInt8)
+    include Fields::S(UInt8)
+
+    property v : UInt8
+
+    include Fields::A(UInt8)
+
+    def initialize(@h : UInt8 = 0, @s : UInt8 = 0, @v : UInt8 = 0, @a : UInt8 = 255)
+    end
+
+    @[AlwaysInline]
+    def value : UInt8
+      @v
+    end
+
+    @[AlwaysInline]
+    def value=(new_value : UInt8)
+      @v = new_value
+    end
+
+  end
+end
